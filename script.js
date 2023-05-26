9db6a1888d5dd0f07007826df340307e86735ea5
cwsprandel <110702891+cwsprandel@users.noreply.github.com>
2023-05-25 20:58:16 -0500

    adding comments for next steps

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const img = document.querySelector('img');
 
 
-async function getForecast(units, days, location) {
+async function getWeather(units, days, location) {
     if (location) {
         locationString = location;     
     } else {
@@ -11,21 +11,21 @@ async function getForecast(units, days, location) {
     let fetchString = `http://api.weatherapi.com/v1/forecast.json?key=dfaaf2e6c86e418fa46122345231105&q=${locationString}&days=${forecastInt}&aqi=yes&alerts=yes`;
     const response = await fetch(fetchString, {mode: 'cors'});
     const weatherData = await response.json();
-    const currentWeatherForecast = weatherData.current;
+    const currentWeather = weatherData.current;
     console.log(weatherData);
 
     if (units === 'f') {
-        currentTemp = currentWeatherForecast.temp_f;
+        currentTemp = currentWeather.temp_f;
     } else if(units === 'c') {
-        currentTemp = currentWeatherForecast.temp_c;
+        currentTemp = currentWeather.temp_c;
     } else {
         console.log('please enter a correct unit');
     }
     
-    img.src = currentWeatherForecast.condition.icon;
+    img.src = currentWeather.condition.icon;
 };
 
-getForecast('f', 3, 'Houston');
+getWeather('f', 3, 'Houston');
 
 
 
