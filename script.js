4c51830be0eeb4122378f228609976634cabacee
cwsprandel <110702891+cwsprandel@users.noreply.github.com>
2023-05-25 20:39:49 -0500

    Renaming some of the Variables

--- script.js
+++ script.js
@@ -11,24 +11,25 @@ async function getForecast(units, days, location) {
     let fetchString = `http://api.weatherapi.com/v1/forecast.json?key=dfaaf2e6c86e418fa46122345231105&q=${locationString}&days=${forecastInt}&aqi=yes&alerts=yes`;
     const response = await fetch(fetchString, {mode: 'cors'});
     const weatherData = await response.json();
-    const currentWeather = weatherData.current;
+    const currentWeatherForecast = weatherData.current;
     console.log(weatherData);
 
     if (units === 'f') {
-        currentTemp = currentWeather.temp_f;
+        currentTemp = currentWeatherForecast.temp_f;
     } else if(units === 'c') {
-        currentTemp = currentWeather.temp_c;
+        currentTemp = currentWeatherForecast.temp_c;
     } else {
         console.log('please enter a correct unit');
     }
     
-    img.src = currentWeather.condition.icon;
-    console.log(currentTemp);
+    img.src = currentWeatherForecast.condition.icon;
 };
+
+getForecast('f', 3, 'Houston');
+
+
 
 // the code below can print out from an async function. may not need this??
 // (async () => {
 //     console.log((await getWeather()).weatherData.current.temp_f)
 // })()
-
-getForecast('f', 3, 'Houston');
