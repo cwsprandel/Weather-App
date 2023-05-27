07d77aa58d384330f5a0d687c64e88d5e7a1dfd9
cwsprandel <110702891+cwsprandel@users.noreply.github.com>
2023-05-26 21:00:18 -0500

    Getting Initial Data from API

--- script.js
+++ script.js
@@ -23,6 +23,9 @@ async function getWeather(units, days, location, type) {
         currentTemp = currentWeather.temp_f;
     }
     
+    let weatherObject = {icon: currentWeather.condition.icon, lowTemperature: weatherData.forecast.forecastday[0].day.mintemp_f, highTemperature: weatherData.forecast.forecastday[0].day.maxtemp_f, humidity: currentWeather.humidity, chanceOfRain: weatherData.forecast.forecastday[0].day.daily_chance_of_rain}
+    createWeatherCard(weatherObject);
+
     img.src = currentWeather.condition.icon;
 };
 
@@ -35,7 +38,8 @@ const temperatureSelector = document.getElementById("temperatureSelector");
 
 const refreshButton = document.getElementById("refreshData");
 refreshButton.addEventListener("click", () => {
-    getInputs();
+    //getInputs();
+    createWeatherCard();
 })
 
 function getInputs() {
@@ -44,30 +48,48 @@ function getInputs() {
     let weatherLocation = weatherLocationElement.value;
 }
 
-function createWeatherCard() {
+function createWeatherCard(weatherObject) {
+    const weatherContainer = document.getElementById("weatherContainer");
+
     const weatherDataItem = document.createElement("div");
     weatherDataItem.setAttribute("class", "weatherDataItem");
 
     const weatherIcon = document.createElement("img");
     weatherIcon.setAttribute("class", "weatherIcon");
+    weatherIcon.src = weatherObject.icon;
 
     const lowTempLabel = document.createElement("h2");
     lowTempLabel.setAttribute("class", "lowTempLabel");
+    lowTempLabel.textContent = "Low";
 
     const highTempLabel = document.createElement("h2");
     highTempLabel.setAttribute("class", "highTempLabel");
+    highTempLabel.textContent = "High";
 
-    const lowTemp = document.createElement("h1");
+    const lowTemp = document.createElement("h2");
     lowTemp.setAttribute("class", "lowTemp");
+    lowTemp.textContent = weatherObject.lowTemperature;
 
-    const highTemp = document.createElement("h1");
+    const highTemp = document.createElement("h2");
     highTemp.setAttribute("class", "highTemp");
+    highTemp.textContent = weatherObject.highTemperature;
 
     const humidity = document.createElement("p");
     humidity.setAttribute("class", "humidity");
+    humidity.textContent = "Humidity: " + weatherObject.humidity + "%";
 
     const rain = document.createElement("p");
     rain.setAttribute("class", "rain");
+    rain.textContent = "Rain: " + weatherObject.chanceOfRain + "%";
+
+    weatherDataItem.appendChild(weatherIcon);
+    weatherDataItem.appendChild(lowTempLabel);
+    weatherDataItem.appendChild(highTempLabel);
+    weatherDataItem.appendChild(lowTemp);
+    weatherDataItem.appendChild(highTemp);
+    weatherDataItem.appendChild(humidity);
+    weatherDataItem.appendChild(rain);
+    weatherContainer.appendChild(weatherDataItem);
 }
 
 // the code below can print out from an async function. may not need this??
